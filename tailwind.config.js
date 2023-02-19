7600e55f7964b39d1814330c217249b6c2e656e4
seda <sedat@sedat-MBP.fritz.box>
2023-02-19 22:12:15 +0100

    add home page with gallery

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors:{
+      darkGray:'#3B3B40', 
+      lightGray:'#D9D9D9',
+      cherryRed:"#A64153",
+      pinkRed:'#F2637E',
+      peach:'#F26B76',
+      white: '#FFFFFF',
+      black:'#000000'
+    },
+    extend: {
+      opacity:{
+        75:'75%',
+        100:'100%'
+      },
+      fontFamily: {
+        'avenir': ['Avenir', 'sans-serif']
+      },
+    },
   },
   plugins: [],
-}
+};
