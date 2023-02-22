54de365c12a847667ffdaaef0cf34bc504ff4520
seda <sedat@sedat-MBP.fritz.box>
2023-02-22 19:43:20 +0100

    add overlay and off-canvas menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,24 @@ module.exports = {
         75:'75%',
         100:'100%'
       },
+      padding:{
+        4.5:'18px',
+      },
       fontFamily: {
         'avenir': ['Avenir', 'sans-serif']
       },
+      fontSize:{
+        '4.5xl':'40px'
+      },
+      backgroundImage: {
+        'menu': "url('/images/menu_bg.svg')",
+      },
+      flex: {
+        '0': '0 0 auto'
+      },
+      backdropBlur: {
+        sm: '4px',
+      }
     },
   },
   plugins: [],
