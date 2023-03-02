3aa12aa6e11ac7371997a4a985b5fe99545d213c
seda <sedat@sedat-MBP.fritz.box>
2023-03-02 21:21:48 +0100

    purgecss fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge:{
   content: ["./**/*.{html,js}"],
   theme: {
     colors:{
@@ -24,4 +25,5 @@ module.exports = {
     },
   },
   plugins: [],
+}
 };
